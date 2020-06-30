cd75ac4abb5903557b9005f4b0f8ad40563ac603
ml <2652592612@qq.com>
2020-06-30 12:02:18 +0800

    初始化

---
+++ app/Http/Controllers/Api/SignController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Model\UserModel;
+use Illuminate\Http\Request;
+use App\Model\TokenModel;
+use Illuminate\Support\Facades\Redis;
+
+class SignController extends Controller
+{
+
+
+    public function sign1(){
+        $key='1910';
+        $data=$_GET['data'];
+        $sign=$_GET['sign'];
+        $sign1=sha1($data.$key);
+        if($sign==$sign1){
+            echo '通过';
+        }else{
+            echo 'no';
+        }
+    }
+
+    public function signadd(){
+        $key='1910';
+        $url='http://www.1910.com/api/signadd';
+        $data=[
+            'user_name'=>'mss',
+            'user_age'=>5655
+        ];
+        $sign=sha1(json_encode($data).$key).json_encode($data);
+        $ch=curl_init();
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_POST,1);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$sign);
+        //curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_exec($ch);
+        $errno=curl_errno($ch);
+        $errmsg=curl_error($ch);
+        if($errno){
+            echo '错误码：'.$errno;echo '</br>';
+            var_dump($errmsg);
+            die;
+        }
+        curl_close($ch);
+
+
+    }
+
+    //加密
+    public function encrypt(){
+       $enc_data=$_POST['data'];
+        $sign=$_POST['sign'];
+
+        $method='AES-256-CBC';
+        $key='1910api';
+        $iv='hellwohellwosjsj';
+        $sign2=sha1($enc_data,$key);
+        if($sign==$sign2){
+            echo '验签成功';echo '</br>';
+        }else{
+            echo '验签失败';die;
+        }
+        $dec_data=openssl_decrypt($enc_data,$method,$key,OPENSSL_RAW_DATA,$iv);
+        echo $dec_data;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/info', function () {
+    phpinfo();
+});
+Route::get('test',"TestController@hello");
+Route::get('goods/index',"Goods\GoodsController@index");
+Route::get('redis',"TestController@redis");
+
+
+Route::get('user/reg',"Goods\GoodsController@reg"); //注册前台
+Route::post('user/regad',"Goods\GoodsController@regad"); //注册后台
+
+Route::get('user/login',"Goods\GoodsController@login"); //前台登录
+Route::post('user/loginadd',"Goods\GoodsController@loginadd"); //后台登录
+
+Route::get('user/conter',"Goods\GoodsController@conter"); //个人中心
+
+
+
+
+//Api
+Route::post('api/user/reg',"Api\Usercontroller@regad"); //注册后台
+Route::post('api/user/login',"Api\Usercontroller@loginadd"); //登录后台
+Route::get('api/user/center',"Api\Usercontroller@center")->middleware('checkpri'); //个人中心
+
+
+
+
+//验签
+Route::get('api/sign',"Api\SignController@sign1");
+Route::any('api/signadd',"Api\SignController@signadd");
+
+//加密
+Route::post('enctypt',"Api\SignController@encrypt");
